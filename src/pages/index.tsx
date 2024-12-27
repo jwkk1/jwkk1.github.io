6dcc9165e3829008d03349d2dd110cda8e990bd6
jwkk1 <dog107.2@gmail.com>
2024-12-27 11:25:18 +0900

    feat: post Item 컴포넌트 캡슐화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,52 @@
 import { graphql, PageProps } from 'gatsby'
+import Category from '../components/main/Category'
+import Introduction from '../components/main/introduction'
+import { useState } from 'react'
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
+import PostItem from '../components/main/PostItem'
+import PostList from '../components/main/PostList'
 
 export default function Index({
   data: {
     allContentfulPost: { nodes },
   },
 }: PageProps<Queries.IndexPageQuery>) {
+  const [selectedCategory, setSelectedCategory] = useState<string>('All')
+
+  const categories = nodes.reduce<Record<string, number>>(
+    (categories, post) => {
+      console.log('reduce', categories, post)
+      post.category
+        ?.filter((category): category is string => !!category)
+        .forEach(
+          category => (categories[category] = (categories[category] ?? 0) + 1),
+        )
+
+      return categories
+    },
+    { All: nodes.length },
+  )
+
+  const posts = nodes.filter(
+    ({ category }) =>
+      selectedCategory === 'All' || category?.includes(selectedCategory),
+  )
+
+  console.log(nodes)
+
+  const handleSelect = (category: string) => {
+    setSelectedCategory(category)
+  }
+
   return (
     <>
-      {nodes.map(({ title, slug, date }) => (
-        <div key={slug}>
-          {title} / {date} / {slug}
-        </div>
-      ))}
+      <Introduction />
+      <Category
+        categories={categories}
+        selectedCategory={selectedCategory}
+        handleSelect={handleSelect}
+      />
+      <PostList posts={posts} />
     </>
   )
 }
@@ -21,8 +56,15 @@ export const query = graphql`
     allContentfulPost(sort: { date: DESC }) {
       nodes {
         title
+        category
         slug
         date
+        thumbnail {
+          gatsbyImageData(width: 500)
+        }
+        description {
+          description
+        }
       }
     }
   }
