55597bddf349063523af953ab971db8d0b11831d
jwkk1 <dog107.2@gmail.com>
2024-12-27 14:52:43 +0900

    rename: 파일명 변경

--- src/pages/{contentfulPost.slug}.tsx
+++ src/pages/{contentfulPost.slug}.tsx
@@ -1,5 +1,51 @@
-import { PageProps } from 'gatsby'
+import { PageProps, graphql } from 'gatsby'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+import PostHead from '../components/post/PostHead'
+import PostBody from '../components/post/PostBody'
 
-export default function Post({ params }: PageProps) {
-  return <div>{params.slug}</div>
+export default function Post({
+  data: { contentfulPost },
+}: PageProps<Queries.PostPageQuery>) {
+  console.log(contentfulPost)
+
+  return (
+    <>
+      <PostHead
+        title={contentfulPost?.title as string}
+        category={contentfulPost?.category as string[]}
+        date={contentfulPost?.date as string}
+        thumbnail={
+          contentfulPost?.thumbnail?.gatsbyImageData as IGatsbyImageData
+        }
+      />
+      {/* <PostBody
+        content={contentfulPost?.content as Queries.ContentfulPostContent}
+      /> */}
+    </>
+  )
 }
+
+export const query = graphql`
+  query PostPage($slug: String!) {
+    contentfulPost(slug: { eq: $slug }) {
+      title
+      thumbnail {
+        gatsbyImageData(width: 1000)
+      }
+      category
+      date
+      content {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            title
+            description
+            gatsbyImageData(width: 774)
+            __typename
+          }
+        }
+      }
+    }
+  }
+`
