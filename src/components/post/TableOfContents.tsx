b0eb2305270ee3cd11c9c7cb92e2f108346e0488
jwkk1 <dog107.2@gmail.com>
2024-12-30 09:32:25 +0900

    feat: Header가 있을떄만 TableOfContents 노출

--- src/components/post/TableOfContents.tsx
+++ src/components/post/TableOfContents.tsx
@@ -48,21 +48,22 @@ export default function TableOfContents({
 }: TableOfContentsProps) {
   const { toc, activeId } = useTableOfContents(raw as string)
 
-  return (
-    <Wrapper>
-      <Title>Table of Contents</Title>
-      <Items>
-        {toc.map(({ id, title, depth }) => (
-          <Item
-            to={`#${id}`}
-            key={id}
-            $depth={depth}
-            $focused={id === activeId}
-          >
-            {title}
-          </Item>
-        ))}
-      </Items>
-    </Wrapper>
-  )
+  if (toc.length > 0)
+    return (
+      <Wrapper>
+        <Title>Table of Contents</Title>
+        <Items>
+          {toc.map(({ id, title, depth }) => (
+            <Item
+              to={`#${id}`}
+              key={id}
+              $depth={depth}
+              $focused={id === activeId}
+            >
+              {title}
+            </Item>
+          ))}
+        </Items>
+      </Wrapper>
+    )
 }
